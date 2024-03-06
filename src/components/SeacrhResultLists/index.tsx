557c387100c18ff2df07b67715c5bb287803faa0
Qasim Fida <qasimfida0054@gmail.com>
2024-03-06 22:37:06 +0500

    UI fixes.

--- src/components/SeacrhResultLists/index.tsx
+++ src/components/SeacrhResultLists/index.tsx
@@ -7,21 +7,23 @@ import { useSearch } from "../../context/SearchContext";
 export const SearchResultsList: React.FC = () => {
   const { results, search } = useSearch();
   const res = filterCategoriesAndServices(results, search);
-  console.log(res, "result");
   const totalHeight = res.length > 0 ? "h-[330px]" : "h-auto";
   if (!search) {
     return null;
   }
   return (
     <div
-      className={`search_lists border  border-primary absolute top-[54px] max-w-[550px] w-[100%] rounded-[10px] bg-white ${totalHeight} flex flex-col shadow-md rounded-lg mt-4 overflow-y-auto scrollbar-thin scrollbar-thumb-red-500 scrollbar-track-gray-200`}
+      className={`wrapper pr-[6px] border absolute top-[60px] max-w-[550px] w-[100%] rounded-[10px] bg-white border-primary`}
     >
-      {res.length
-        ? res.map((result: any, id: number) => (
+      <div className={`search_lists ${totalHeight}`}>
+        {res.length ? (
+          res.map((result: any, id: number) => (
             <SearchResult result={result} key={id} />
           ))
-        : ""}
-      <p className="p-4">No Results found!</p>
+        ) : (
+          <p className="p-4">No Results found!</p>
+        )}
+      </div>
     </div>
   );
 };
--- src/pages/Service/index.jsx
+++ src/pages/Service/index.jsx
@@ -4,13 +4,12 @@ import { useParams } from "react-router-dom";
 import { slugToString } from "../../helpers";
 
 const Service = () => {
-
   let { service } = useParams();
-  
+
   return (
     <div>
       <Header />
-      <div className="p-4">{slugToString(service)}</div>
+      <div className="p-4 text-primary font-bold	capitalize text-3xl mt-5">{slugToString(service)}</div>
     </div>
   );
 };
